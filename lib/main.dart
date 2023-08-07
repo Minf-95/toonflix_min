0cf3cee7b151c410787f8bacca43b9975ba372ab
MinSeokOh <msoh@mrpsoft.co.kr>
2023-08-08 00:50:54 +0900

    first Push

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App()); // runApp은 앱을 실행시키는 역할을 하고, Widget을 인자로 받는다.
+}
+
+class App extends StatelessWidget {
+  // runApp에 전달할 위젯을 정의하는데, Class를 Widget으로 바꿔주기 위해 가장 기초적이고 쉬운 Stateless 위젯을 상속 받는다.
+
+}
